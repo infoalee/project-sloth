d4402af2c0a1bb9ad5b9d4e3e5824956b6d89412
infoalee <infoalee@Alees-MacBook-Pro.local>
2017-08-04 10:59:43 +0700

    update2

--- index1.php
+++ index1.php
@@ -550,7 +550,7 @@ desired effect
      user experience. Slimscroll is required when using the
      fixed layout. -->
 <script type="text/javascript">
- $('.ajax-content').load('booking1.php');
+ $('.ajax-content').load('booking.php');
 	// To make Pace works on Ajax calls
 	$(document).ajaxStart(function() { Pace.restart(); });
     $('.ajax').click(function(){
